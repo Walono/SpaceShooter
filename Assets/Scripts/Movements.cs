be772f4f46d52fe61d57589289e9a50905676785
Fabien Zellweger <fabien.zellweger@gmail.com>
2020-12-11 12:39:22 +0000

    Ensure the ship stays on camera view.

--- Assets/Scripts/Movements.cs
+++ Assets/Scripts/Movements.cs
@@ -7,14 +7,25 @@ public class Movements : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 newPose = transform.position;
+
         if (Input.GetKey("up") || Input.GetKey("w"))
         {
-            transform.position = transform.position + new Vector3(0f,0.05f,0f);
+            newPose += new Vector3(0f, 0.1f, 0f);
         }
         if (Input.GetKey("down") || Input.GetKey("s"))
         {
-            transform.position = transform.position - new Vector3(0f, 0.05f, 0f);
+            newPose -= new Vector3(0f, 0.1f, 0f);
         }
         // TODO: Add left and right movements.
+
+        Vector3 screenPoint = Camera.main.WorldToViewportPoint(newPose);
+        bool onScreen = screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+
+        if (onScreen)
+        {
+            transform.position = newPose;
+        }
     }
 }
