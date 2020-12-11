2cf611db9621f2ba5c4168e73656e6f7118679f1
Fabien Zellweger <fabien.zellweger@gmail.com>
2020-12-11 11:41:49 +0000

    Setup spaceship with up and down movements.

---
+++ Assets/Scripts/Movements.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movements : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up") || Input.GetKey("w"))
+        {
+            transform.position = transform.position + new Vector3(0f,0.05f,0f);
+        }
+        if (Input.GetKey("down") || Input.GetKey("s"))
+        {
+            transform.position = transform.position - new Vector3(0f, 0.05f, 0f);
+        }
+        // TODO: Add left and right movements.
+    }
+}
