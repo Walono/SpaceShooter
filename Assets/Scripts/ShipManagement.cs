5c6748baa4e6148f06883e4b149b19d5a2788fc2
Fabien Zellweger <fabien.zellweger@gmail.com>
2020-12-12 12:14:30 +0000

    Add singleton ship manager.

---
+++ Assets/Scripts/ShipManagement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ShipManagement : MonoBehaviour
+{
+    private static ShipManagement _instance;
+    public static ShipManagement Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
