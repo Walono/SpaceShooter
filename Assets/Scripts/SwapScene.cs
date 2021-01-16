cf55990758b8d73165085ebbcc51789454abd9e0
Fabien Zellweger <Fabien.zellweger@gmail.com>
2021-01-16 13:21:27 +0000

    Add Game Over scene.

---
+++ Assets/Scripts/SwapScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwapScene : MonoBehaviour
+{
+    public void SwapToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
